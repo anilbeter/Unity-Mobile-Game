3ba41c4552c5caab904ea81478c286813d25543c
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-23 15:14:29 +0300

    setup LaserBullet script

--- Assets/Scripts/LaserBullet.cs
+++ Assets/Scripts/LaserBullet.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class LaserBullet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed;
+    [SerializeField] private float damage;
+    [SerializeField] private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb.velocity = transform.up * speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        // every enemy type has the Enemy script as a parent class
+        Enemy enemy = other.GetComponent<Enemy>();
+        enemy.TakeDamage(damage);
+        Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
