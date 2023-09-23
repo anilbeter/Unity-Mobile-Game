06b8500e8b8217132beb86f6dcfa382c2c58deda
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-23 14:32:19 +0300

    declare variables for MeteorSpawner script

--- Assets/Scripts/EnemyScripts/MeteorSpawner.cs
+++ Assets/Scripts/EnemyScripts/MeteorSpawner.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class MeteorSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // this array will contains all meteor prefabs
+    [SerializeField] private GameObject[] meteor;
+    [SerializeField] private float spawnTime;
+    private float timer;
+    private int i;
+
+    private Camera mainCam;
+    private float maxLeft;
+    private float maxRight;
+    private float yPos;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
