435f3ea8e5e3eab7e1b1c5f4e0d0840b64724b13
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-23 14:36:46 +0300

    create IEnumerator SetBoundaries() function

--- Assets/Scripts/EnemyScripts/MeteorSpawner.cs
+++ Assets/Scripts/EnemyScripts/MeteorSpawner.cs
@@ -18,11 +18,22 @@ public class MeteorSpawner : MonoBehaviour
     void Start()
     {
         mainCam = Camera.main;
+        StartCoroutine(SetBoundaries());
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private IEnumerator SetBoundaries()
+    {
+        yield return new WaitForSeconds(0.4f);
+        maxLeft = mainCam.ViewportToWorldPoint(new Vector2(0.15f, 0)).x;
+        maxRight = mainCam.ViewportToWorldPoint(new Vector2(0.85f, 0)).x;
+
+        // yPos needs to be outside of the viewport/screen. So I need something that greater than 1
+        yPos = mainCam.ViewportToWorldPoint(new Vector2(0, 1.1f)).y;
     }
 }
