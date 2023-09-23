292f1651fd5106e87359c479081376946b3f8571
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-23 13:35:19 +0300

    destroy player when meteor collides with player

--- Assets/Scripts/EnemyScripts/Meteor.cs
+++ Assets/Scripts/EnemyScripts/Meteor.cs
@@ -29,4 +29,12 @@ public class Meteor : Enemy
     {
 
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
 }
