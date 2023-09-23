c7264c4f4bd8cf1eff0b25039c412b3b734ca02d
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-23 12:32:06 +0300

    update touch controls for samsung devices

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -27,26 +27,36 @@ public class PlayerControls : MonoBehaviour
     void Update()
     {
         // fingers[0] -> first finger
-        if (Touch.fingers[0].isActive)
+        if (Touch.activeTouches.Count > 0)
         {
-            Touch myTouch = Touch.activeTouches[0];
-            Vector3 touchPos = myTouch.screenPosition;
-            touchPos = mainCam.ScreenToWorldPoint(touchPos);
+            if (Touch.activeTouches[0].finger.index == 0)
+            {
+                Touch myTouch = Touch.activeTouches[0];
+                Vector3 touchPos = myTouch.screenPosition;
 
-            if (Touch.activeTouches[0].phase == TouchPhase.Began)
-            {
-                // distance between touch and player
-                offset = touchPos - transform.position;
-            }
+#if UNITY_EDITOR
 
-            // moved -> player moves finger on the screen
-            if (Touch.activeTouches[0].phase == TouchPhase.Moved)
-            {
-                transform.position = new Vector3(touchPos.x - offset.x, touchPos.y - offset.y, 0);
-            }
-            if (Touch.activeTouches[0].phase == TouchPhase.Stationary)
-            {
-                transform.position = new Vector3(touchPos.x - offset.x, touchPos.y - offset.y, 0);
+                if (touchPos.x == Mathf.Infinity)
+                    return;
+
+#endif
+                touchPos = mainCam.ScreenToWorldPoint(touchPos);
+
+                if (Touch.activeTouches[0].phase == TouchPhase.Began)
+                {
+                    // distance between touch and player
+                    offset = touchPos - transform.position;
+                }
+
+                // moved -> player moves finger on the screen
+                if (Touch.activeTouches[0].phase == TouchPhase.Moved)
+                {
+                    transform.position = new Vector3(touchPos.x - offset.x, touchPos.y - offset.y, 0);
+                }
+                if (Touch.activeTouches[0].phase == TouchPhase.Stationary)
+                {
+                    transform.position = new Vector3(touchPos.x - offset.x, touchPos.y - offset.y, 0);
+                }
             }
 
             transform.position = new Vector3(Mathf.Clamp(transform.position.x, maxLeft, maxRight), Mathf.Clamp(transform.position.y, maxDown, maxUp), 0);
