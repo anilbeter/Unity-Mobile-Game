8dde948b655e401c6054ff14f8f07d343549a65f
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-23 15:05:38 +0300

    setup PlayerShooting script

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject laserBullet;
+    [SerializeField] private Transform basicShootPoint;
+
+    [SerializeField] private float shootingInterval;
+    private float intervalReset;
+
     void Start()
     {
-        
+        intervalReset = shootingInterval;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        shootingInterval -= Time.deltaTime;
+        if (shootingInterval <= 0)
+        {
+            Shoot();
+            shootingInterval = intervalReset;
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(laserBullet, basicShootPoint.position, Quaternion.identity);
+        // Quaternion.identity -> there will be no rotation for bullets
     }
 }
+
