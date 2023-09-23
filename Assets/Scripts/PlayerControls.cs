efce63adc8bb957ab9670a4dada0c3ae1bdea36b
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-23 10:58:47 +0300

    move player with touch [setup PlayerControls script]

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -2,18 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using TouchPhase = UnityEngine.InputSystem.TouchPhase;
 
 public class PlayerControls : MonoBehaviour
 {
+    private Camera mainCam;
+    private Vector3 offset;
 
     void Start()
     {
-
+        mainCam = Camera.main;
     }
 
 
     void Update()
     {
+        // fingers[0] -> first finger
+        if (Touch.fingers[0].isActive)
+        {
+            Touch myTouch = Touch.activeTouches[0];
+            Vector3 touchPos = myTouch.screenPosition;
+            touchPos = mainCam.ScreenToWorldPoint(touchPos);
 
+            if (Touch.activeTouches[0].phase == TouchPhase.Began)
+            {
+                // distance between touch and player
+                offset = touchPos - transform.position;
+            }
+
+            // moved -> player moves finger on the screen
+            if (Touch.activeTouches[0].phase == TouchPhase.Moved)
+            {
+                transform.position = new Vector3(touchPos.x - offset.x, touchPos.y - offset.y, 0);
+            }
+            if (Touch.activeTouches[0].phase == TouchPhase.Stationary)
+            {
+                transform.position = new Vector3(touchPos.x - offset.x, touchPos.y - offset.y, 0);
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+    }
+
+    private void OnDisable()
+    {
+        EnhancedTouchSupport.Disable();
     }
 }
