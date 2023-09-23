744ac02fc2e7d30f0fd9d59fcf5f55b75ef686c8
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-23 10:31:06 +0300

    create PlayerControls script

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
