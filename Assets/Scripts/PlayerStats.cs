c45f9754b0f380e9b8c5d57269302fef0fb44836
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-24 09:16:09 +0300

    setup PlayerStats script for damage animation & add IEnumarator function

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -10,6 +10,9 @@ public class PlayerStats : MonoBehaviour
 
     [SerializeField] private Image healthFill;
     [SerializeField] private GameObject explosionPrefab;
+    [SerializeField] private Animator anim;
+
+    private bool canPlayAnim = true;
 
     void Start()
     {
@@ -21,10 +24,22 @@ public class PlayerStats : MonoBehaviour
     {
         health -= damage;
         healthFill.fillAmount = health / maxHealth;
+        if (canPlayAnim)
+        {
+            anim.SetTrigger("Damage");
+            StartCoroutine(AntiSpamAnimation());
+        }
         if (health <= 0)
         {
             Instantiate(explosionPrefab, transform.position, transform.rotation);
             Destroy(gameObject);
         }
+    }
+
+    private IEnumerator AntiSpamAnimation()
+    {
+        canPlayAnim = false;
+        yield return new WaitForSeconds(0.15f);
+        canPlayAnim = true;
     }
 }
