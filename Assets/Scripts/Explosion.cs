bfdf355fe72ee5b161a7a19cb75acc9fda178fb0
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-24 08:54:12 +0300

    add Destroy function in Explosion script

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        Destroy(gameObject, 2);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
