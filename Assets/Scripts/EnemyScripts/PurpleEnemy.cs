8e515fe5c3c49d7d3d5fdfbef81ac5eea11c49d8
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-24 09:37:45 +0300

    setup PurpleEnemy script

--- Assets/Scripts/EnemyScripts/PurpleEnemy.cs
+++ Assets/Scripts/EnemyScripts/PurpleEnemy.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PurpleEnemy : Enemy
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed;
+
+    private float shootTimer = 0;
+    [SerializeField] private float shootInterval;
+
+    [SerializeField] private Transform leftCannon;
+    [SerializeField] private Transform rightCannon;
+
+    [SerializeField] private GameObject bulletPrefab;
+
     void Start()
     {
-
+        rb.velocity = Vector2.down * speed;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        shootTimer += Time.deltaTime;
+        if (shootTimer >= shootInterval)
+        {
+            Instantiate(bulletPrefab, leftCannon.position, Quaternion.identity);
+            Instantiate(bulletPrefab, rightCannon.position, Quaternion.identity);
+            shootTimer = 0;
+        }
+    }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // damage variable is inherited from parent class
+            other.GetComponent<PlayerStats>().PlayerTakeDamage(damage);
+            // explosionPrefab variable is inherited from parent class
+            Instantiate(explosionPrefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+    public override void HurtSequence()
+    {
+        // animation
+    }
+
+    public override void DeathSequence()
+    {
+        Instantiate(explosionPrefab, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
