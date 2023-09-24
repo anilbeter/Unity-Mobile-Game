59d9d3f5d30d2dba75da145150cb1c686a7a563f
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-24 10:56:16 +0300

    setup EnemySpawner script

--- Assets/Scripts/EnemyScripts/EnemySpawner.cs
+++ Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Camera mainCam;
+    private float maxLeft;
+    private float maxRight;
+    private float yPos;
+
+    [Header("Enemy Prefabs")]
+    [SerializeField] private GameObject[] enemy;
+
+    private float enemyTimer;
+    [Space(15)]
+    [SerializeField] private float enemySpawnTime;
+
     void Start()
     {
-        
+        mainCam = Camera.main;
+        StartCoroutine(SetBoundaries());
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EnemySpawn();
+    }
+
+    private void EnemySpawn()
+    {
+        enemyTimer += Time.deltaTime;
+        if (enemyTimer >= enemySpawnTime)
+        {
+            int randomPick = Random.Range(0, enemy.Length);
+            Instantiate(enemy[randomPick], new Vector3(Random.Range(maxRight, maxLeft), yPos, 0), Quaternion.identity);
+            enemyTimer = 0;
+        }
+    }
+
+    private IEnumerator SetBoundaries()
+    {
+        yield return new WaitForSeconds(0.4f);
+        maxLeft = mainCam.ViewportToWorldPoint(new Vector2(0.15f, 0)).x;
+        maxRight = mainCam.ViewportToWorldPoint(new Vector2(0.85f, 0)).x;
+
+        yPos = mainCam.ViewportToWorldPoint(new Vector2(0, 1.1f)).y;
     }
 }
