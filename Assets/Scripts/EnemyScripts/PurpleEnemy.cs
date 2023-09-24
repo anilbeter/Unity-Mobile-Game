6518060a155e681536ac818e3a801bc6abb6c7fa
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-24 09:23:31 +0300

    make child class of Enemy

--- Assets/Scripts/EnemyScripts/PurpleEnemy.cs
+++ Assets/Scripts/EnemyScripts/PurpleEnemy.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PurpleEnemy : MonoBehaviour
+public class PurpleEnemy : Enemy
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
