d3742d819be3cfeffa306f0932f8279b3405f6b7
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-24 09:44:08 +0300

    setup PurpleBullet script

--- Assets/Scripts/EnemyScripts/PurpleBullet.cs
+++ Assets/Scripts/EnemyScripts/PurpleBullet.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PurpleBullet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed;
+    [SerializeField] private float damage;
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.down * speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerStats>().PlayerTakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
