c634251d5dec0813ea282acc89cd3e25530e448f
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-25 12:02:35 +0300

    setup ScoreRegister script

--- Assets/Scripts/UI Scripts/ScoreRegister.cs
+++ Assets/Scripts/UI Scripts/ScoreRegister.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreRegister : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        TextMeshProUGUI textForRegistration = GetComponent<TextMeshProUGUI>();
+        EndGameManager.endManager.RegisterScoreText(textForRegistration);
+        textForRegistration.text = "Score: 0";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
