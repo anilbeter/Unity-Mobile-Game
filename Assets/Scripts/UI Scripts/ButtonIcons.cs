1dd6c54f6584ab650420ce2c4bd6f03a5ff54201
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-25 15:36:12 +0300

    setup ButtonIcons script

--- Assets/Scripts/UI Scripts/ButtonIcons.cs
+++ Assets/Scripts/UI Scripts/ButtonIcons.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Buttonıcons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button[] lvlButton;
+    [SerializeField] private Sprite unlockedIcon;
+    [SerializeField] private Sprite lockedIcon;
+    [SerializeField] private int firstLevelIndex;
+
+    private void Awake()
     {
-        
+        int unlockedLvl = PlayerPrefs.GetInt(EndGameManager.endManager.lvlUnlock, firstLevelIndex);
+        for (int i = 0; i < lvlButton.Length; i++)
+        {
+            if (i + firstLevelIndex <= unlockedLvl)
+            {
+                lvlButton[i].interactable = true;
+                lvlButton[i].image.sprite = unlockedIcon;
+                TextMeshProUGUI textButton = lvlButton[i].GetComponentInChildren<TextMeshProUGUI>();
+                textButton.text = (i + 1).ToString();
+                textButton.enabled = true;
+            }
+            else
+            {
+                lvlButton[i].interactable = false;
+                lvlButton[i].image.sprite = lockedIcon;
+                lvlButton[i].GetComponentInChildren<TextMeshProUGUI>().enabled = false;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
