2f69226e6bcbe743a7bf25994f223aafadfea484
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-25 14:29:34 +0300

    setup ScoreDisplay script

--- Assets/Scripts/UI Scripts/ScoreDisplay.cs
+++ Assets/Scripts/UI Scripts/ScoreDisplay.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ScoreDisplay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI highScoreText;
+
+    private void OnEnable()
     {
-        
+        int score = PlayerPrefs.GetInt("Score" + SceneManager.GetActiveScene().name, 0);
+        scoreText.text = "Score: " + score.ToString();
+
+        int highScore = PlayerPrefs.GetInt("HighScore" + SceneManager.GetActiveScene().name, 0);
+        highScoreText.text = "Highscore: " + highScore.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
