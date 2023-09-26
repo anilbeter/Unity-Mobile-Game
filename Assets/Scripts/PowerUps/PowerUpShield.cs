91e115b7278894b05188c70cf041d2a20f5696ea
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-26 11:02:14 +0300

    setup PowerUpShield script

--- Assets/Scripts/PowerUps/PowerUpShield.cs
+++ Assets/Scripts/PowerUps/PowerUpShield.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class PowerUpShield : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Player"))
+        {
+            PlayerShieldActivator playerShieldActivator = other.GetComponent<PlayerShieldActivator>();
+            playerShieldActivator.ActivateShield();
+            Destroy(gameObject);
+        }
     }
 }
