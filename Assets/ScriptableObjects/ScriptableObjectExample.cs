bbe06dc9c7f0124c83ebef9b1731c60cba904ccf
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-26 10:00:41 +0300

    add randomness to script

--- Assets/ScriptableObjects/ScriptableObjectExample.cs
+++ Assets/ScriptableObjects/ScriptableObjectExample.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 [CreateAssetMenu(menuName = "ScriptableObject/PowerUpSpawner", fileName = "Spawner")]
 
 public class ScriptableObjectExample : ScriptableObject
 {
+    public int spawnThreshold;
     public GameObject[] powerUp;
 
     public void SpawnPowerUp(Vector3 spawnPos)
     {
-        int randomPowerUp = Random.Range(0, powerUp.Length);
-        Instantiate(powerUp[randomPowerUp], spawnPos, Quaternion.identity);
+        int randomChance = Random.Range(0, 100);
+        if (randomChance > spawnThreshold)
+        {
+            int randomPowerUp = Random.Range(0, powerUp.Length);
+            Instantiate(powerUp[randomPowerUp], spawnPos, Quaternion.identity);
+        }
     }
 }
