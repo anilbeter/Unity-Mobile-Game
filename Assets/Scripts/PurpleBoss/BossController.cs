4cd3676c2e3706cdee2640e8ae8f5e1d8710428e
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-27 10:16:08 +0300

    create ChangeState function that uses switch function and enum struct

--- Assets/Scripts/PurpleBoss/BossController.cs
+++ Assets/Scripts/PurpleBoss/BossController.cs
@@ -2,22 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public enum BossState
 {
     enter,
     fire,
     special,
     death
+    // REVIEW enums
 }
 
 public class BossController : MonoBehaviour
 {
-
-
     void Start()
     {
 
     }
 
-
+    public void ChangeState(BossState state)
+    {
+        switch (state)
+        {
+            case BossState.enter:
+                Debug.Log("Do smth");
+                break;
+            case BossState.fire:
+                Debug.Log("Do smth");
+                break;
+            case BossState.special:
+                Debug.Log("Do smth");
+                break;
+            case BossState.death:
+                Debug.Log("Do smth");
+                break;
+        }
+    }
 }
