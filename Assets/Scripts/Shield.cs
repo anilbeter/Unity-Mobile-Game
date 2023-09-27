dbc9a25b1b28a2606fa01cd444c8349929b80604
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-27 09:14:28 +0300

    update Shield script for Shield UI

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -7,10 +7,48 @@ public class Shield : MonoBehaviour
     private int hitToDestroy = 3;
     public bool protection = false;
 
+    [SerializeField] private GameObject[] shieldBase;
+
     private void OnEnable()
     {
         hitToDestroy = 3;
+        for (int i = 0; i < shieldBase.Length; i++)
+        {
+            shieldBase[i].SetActive(true);
+        }
         protection = true;
+    }
+
+    private void UpdateUI()
+    {
+        switch (hitToDestroy)
+        {
+            case 0:
+                for (int i = 0; i < shieldBase.Length; i++)
+                {
+                    shieldBase[i].SetActive(false);
+                }
+                break;
+            case 1:
+                shieldBase[0].SetActive(true);
+                shieldBase[1].SetActive(false);
+                shieldBase[2].SetActive(false);
+                break;
+            case 2:
+                shieldBase[0].SetActive(true);
+                shieldBase[1].SetActive(true);
+                shieldBase[2].SetActive(false);
+                break;
+            case 3:
+                shieldBase[0].SetActive(true);
+                shieldBase[1].SetActive(true);
+                shieldBase[2].SetActive(true);
+                break;
+            default:
+                Debug.Log("We don't have this case, something should be wrong");
+                break;
+        }
+
     }
 
     private void DamageShield()
@@ -18,14 +56,17 @@ public class Shield : MonoBehaviour
         hitToDestroy -= 1;
         if (hitToDestroy <= 0)
         {
+            hitToDestroy = 0;
             protection = false;
             gameObject.SetActive(false);
         }
+        UpdateUI();
     }
 
     public void RepairShield()
     {
         hitToDestroy = 3;
+        UpdateUI();
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
