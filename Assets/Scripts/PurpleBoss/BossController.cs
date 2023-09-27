e06443e7bbeb888cba9f8036a6fab7aaeac83082
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-27 10:13:26 +0300

    [BossController.cs] create BossState enum

--- Assets/Scripts/PurpleBoss/BossController.cs
+++ Assets/Scripts/PurpleBoss/BossController.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BossState
+{
+    enter,
+    fire,
+    special,
+    death
+}
+
 public class BossController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
