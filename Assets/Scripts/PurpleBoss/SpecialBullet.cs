6951d8088a81fea7a31c11a31773d2ddee678665
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-28 21:15:54 +0300

    setup SpecialBullet script

--- Assets/Scripts/PurpleBoss/SpecialBullet.cs
+++ Assets/Scripts/PurpleBoss/SpecialBullet.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class SpecialBullet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float damage;
+    [SerializeField] private float speed;
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private GameObject miniBullet;
+    [SerializeField] private Transform[] spawnPoint;
+
     void Start()
     {
-        
+        rb.velocity = Vector2.down * speed;
+        StartCoroutine(Explode());
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+    }
+
+    IEnumerator Explode()
+    {
+        float randomExplodeTime = Random.Range(1.5f, 2.5f);
+        yield return new WaitForSeconds(randomExplodeTime);
+        for (int i = 0; i < spawnPoint.Length; i++)
+        {
+            Instantiate(miniBullet, spawnPoint[i].position, spawnPoint[i].rotation);
+        }
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerStats player = GetComponent<PlayerStats>();
+            player.PlayerTakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
